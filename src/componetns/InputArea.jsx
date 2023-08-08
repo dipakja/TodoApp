d2d4c08c6c426a9a930d6b35570c8f1a18c2a616
Dipak Jadhav <104189165+dipakja@users.noreply.github.com>
2023-08-08 18:09:24 +0530

    Add files via upload

---
+++ src/componetns/InputArea.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+// here i lern how can i  manage components.
+// Event handling
+// Pops in reacts
+
+function InputArea(props) {
+  const [inputText, setInputText] = useState("");
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  return (
+    <div className="form">
+      <input onChange={handleChange} type="text" value={inputText} />
+      <button
+        onClick={() => {
+          props.onAdd(inputText);
+          setInputText("");
+        }}
+      >
+        <span>Add</span>
+      </button>
+    </div>
+  );
+}
+
+export default InputArea;
